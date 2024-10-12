523143a1858bc73cd1ba0781fd319a4dcf5b0b2c
ASNR2005 <torresq83@gmail.com>
2024-10-12 16:48:55 -0600

    added all prototypes of functions

--- FunctionalityLibs/Prototypes.h
+++ FunctionalityLibs/Prototypes.h
@@ -6,3 +6,13 @@ void MoveTroughMainMenu(void);
 void DisplayMainMenu(int Option);
 
 #endif
+
+#ifndef FUNCTIONPROTOTYPES_H
+#define FUNCTIONPROTOTYPES_H
+
+int StoreHistoryDataInFile(char *Question, char *Response);
+int ViewHistoryDataInFile();
+void GetGameInstructions();
+void PlayGame();
+
+#endif
